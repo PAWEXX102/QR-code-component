cfdb00798d764e41d38918d12d11fd38a7e88ba4
PAWEXX102 <pawexx102a@wp.pl>
2024-01-27 14:12:35 +0100

    Create

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const outift = Outfit({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${outift.className} bg-Lightgray overflow-hidden flex items-center justify-center h-screen`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
